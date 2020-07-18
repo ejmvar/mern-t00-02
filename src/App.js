fa08585ad6ab2648697af77b6ef4276db29651bd
Eduardo Martinez <ejmvar@gmail.com>
2020-07-18 19:38:48 -0300

    Just a todo list, to restart my brain

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CRUD Feriados
         </a>
       </header>
     </div>
